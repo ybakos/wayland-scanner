b16945f84db30bbb09169bf6d88ccfe290cdfe30
Yong Bakos <ybakos@humanoriented.com>
2016-05-01 10:34:24 -0500

    Reorganize.

--- test/scanner_test.rb
+++ test/scanner_test.rb
@@ -1,9 +1,35 @@
 require 'minitest/autorun'
 
-class ScannerTest < Minitest::Test
+describe Scanner do
 
-  def test_foo
+  before do
+    #TODO
+  end
 
+  describe "when provided a path to a file that does not exist" do
+    it "should exit with a 'file not found' message"
+      #TODO
+    end
+  end
+
+  describe "when provided a protocol xml file that doesn't conform to the dtd" do
+    it "should exit with a 'does not conform' message" do
+      #TODO
+    end
+  end
+
+  describe "when provided with a valid protocol xml file" do
+    it "should generate a corresponding header file" do
+      #TODO
+    end
+  end
+
+  describe "Scanner Benchmark" do
+    if ENV["BENCHMARK"]
+      bench_performance_linear "todo", "benchmark.xml" do |n|
+        100.times { @obj.todo(n) }
+      end
+    end
   end
 
 end
